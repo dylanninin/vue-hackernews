f428e2bfafe0a880f5be1237367f7e3aeabbef20
Dylan <dylanninin@gmail.com>
2016-11-18 17:05:45 +0800

    Update main

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 import router from './router'
+import { sync } from 'vuex-router-sync'
+
+// sync the router with the vuex store
+// this registers `store.state.route`
+sync(store, router)
 
 // Create the app instance
 // here we inject the router to all child components,
 // making them available everywhere as `this.$router`.
 const app = new Vue(Vue.util.extend({
-  router
+  router,
+  store
 }, App))
 
 // Actually mount to DOM
