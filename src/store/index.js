0827553a68273c07d648c121906bffb3915338e8
Dylan <dylanninin@gmail.com>
2016-11-18 17:05:25 +0800

    Add VuexStore

---
+++ src/store/index.js
@@ -0,0 +1,93 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { fetchItems, fetchIdsByType, fetchUser } from './api'
+
+Vue.use(Vuex)
+
+// Reference https://github.com/HackerNews/API
+const store = new Vuex.Store({
+  // state
+  state: {
+    activeType: null,
+    itemsPerPage: 20,
+    items: {},
+    users: {},
+    lists: {
+      top: [],
+      new: [],
+      show: [],
+      ask: [],
+      job: []
+    }
+  },
+
+  // actions
+  actions: {
+    // ensure data for rendering given list type
+    FETCH_LIST: ({ commit, dispatch, state }, { type }) => {
+      commit('SET_ACTIVE_TYPE', { type })
+      return fetchIdsByType(type)
+        .then(ids => commit('SET_LIST', { type, ids }))
+        .then(() => dispatch('ENSURE_ACTIVE_ITEMS'))
+    },
+
+    // ensure all active items are fetched
+    ENSURE_ACTIVE_ITEMS: ({ dispatch, getters }) => {
+      return dispatch('FETCH_ITEMS', {
+        ids: getters.activeIds
+      })
+    },
+
+    FETCH_ITEMS: ({ commit, state }, { ids }) => {
+      return fetchItems(ids).then(items => commit('SET_ITEMS', { items }))
+    },
+
+    FETCH_USER: ({ commit, state }, { id }) => {
+      return fetchUser(id).then(user => commit('SET_USER', { user }))
+    }
+  },
+
+  // mutations
+  mutations: {
+    SET_ACTIVE_TYPE: (state, { type }) => {
+      state.activeType = type
+    },
+
+    SET_LIST: (state, { type, ids }) => {
+      state.lists[type] = ids
+    },
+
+    SET_ITEMS: (state, { items }) => {
+      items.forEach(item => {
+        if (item) {
+          Vue.set(state.items, item.id, item)
+        }
+      })
+    },
+
+    SET_USER: (state, { user }) => {
+      Vue.set(state.users, user.id, user)
+    }
+  },
+
+  // getters
+  getters: {
+    activeIds (state) {
+      const { activeType, itemsPerPage, lists } = state
+      const page = Number(state.route.params.page) || 1
+      if (activeType) {
+        const start = (page - 1) * itemsPerPage
+        const end = page * itemsPerPage
+        return lists[activeType].slice(start, end)
+      } else {
+        return []
+      }
+    },
+
+    activeItems (state, getters) {
+      return getters.activeIds.map(id => state.items[id].filter(_ => _))
+    }
+  }
+})
+
+export default store
