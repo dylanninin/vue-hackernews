a9b4249623605e1216db8503358018b1a0e98a72
Dylan <dylanninin@gmail.com>
2016-11-19 11:14:37 +0800

    Remove preFetch

--- src/views/CreateListView.js
+++ src/views/CreateListView.js
@@ -3,9 +3,6 @@ import ItemList from '../components/ItemList.vue'
 export function createListView (type) {
   return {
     name: `${type}-stories-view`,
-    preFetch (store) {
-      return store.dispatch('FETCH_LIST', { type })
-    },
     render (h) {
       return h(ItemList, { props: { type } })
     }
